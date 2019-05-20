97e5e2be217c13dc099f0192729e682c7270dcfa
MatiasDurbano <matiasgabrieldurbano@gmail.com>
2019-05-20 12:55:05 -0300

    solucion de error en la cual la libreria de nfc crasheaba al leer muchas tarjetas

--- lopy4/NfcReader.py
+++ lopy4/NfcReader.py
@@ -11,7 +11,7 @@ import struct
 class NfcReader:
     #Reemplazar por  datos de bd
     VALID_CARDS = [[0xF4, 0xFE, 0xAB, 0x56],
-                   [0x04, 0x2D, 0x7F, 0x56,],
+                   [0x04, 0x2D, 0x7F, 0x56],
                    [0x14, 0x88, 0x1B, 0x00]]
 
     #defino colores para ver si la tarjeta es valida o no
@@ -24,30 +24,24 @@ class NfcReader:
     def __init__(self):
         self.py = Pyscan()
         self.nfc = MFRC630(self.py)
+        self.nfc.mfrc630_cmd_init()
 
     def check_card(self,uid,len):
-         return self.VALID_CARDS.count(uid[:len])
+        return self.VALID_CARDS.count(uid[:len])
 
     def discovery_loop(self):
-        while True:
-            print("Esperando...")
-            # Send REQA for ISO14443A card type
-            atqa = self.nfc.mfrc630_iso14443a_WUPA_REQA(self.nfc.MFRC630_ISO14443_CMD_REQA)
-            if (atqa != 0):
+        atqa = self.nfc.mfrc630_iso14443a_WUPA_REQA(self.nfc.MFRC630_ISO14443_CMD_REQA)
+        ret = 0
+        if (atqa != 0):
                 # A card has been detected, read UID
-                uid = bytearray(10)
-                uid_len = self.nfc.mfrc630_iso14443a_select(uid)
-                print(self.nfc.format_block(uid, uid_len))
-                if (uid_len > 0):
-                    print(uid)
+            uid = bytearray(10)
+            uid_len = self.nfc.mfrc630_iso14443a_select(uid)
+            print(self.nfc.format_block(uid, uid_len))
+            if (uid_len > 0):
+                print(uid)
                     #aca deberia fijarme si la tarjeta es una de las que esta en la bd
-                    if (self.check_card(list(uid),uid_len)>0):
-                        pycom.rgbled(self.RGB_GREEN)
-                    else:
-                        pycom.rgbled(self.RGB_RED)
-            else:
-                # No card detected
-                pycom.rgbled(self.RGB_BLUE)
-            self.nfc.mfrc630_cmd_reset()
-            time.sleep(.5)
-            self.nfc.mfrc630_cmd_init()
+                if (self.check_card(list(uid),uid_len)>0):
+                    ret=1
+                else:
+                    ret=2
+        return ret
--- lopy4/main.py
+++ lopy4/main.py
@@ -1,9 +1,15 @@
+from lib.pyscan import Pyscan
+from lib.MFRC630 import MFRC630
+import _thread
+import time
 from NfcReader import *
 
 def main():
-    pycom.heartbeat(False)
-    nfcReader = NfcReader()
-    nfcReader.discovery_loop()
+  nfcReader = NfcReader()
+  while True:
+      print(nfcReader.discovery_loop())
+      time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
